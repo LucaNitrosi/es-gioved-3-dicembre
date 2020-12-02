fb8afd781609e6ac448733639308f0698ad54eb2
LucaNitrosi <74323395+LucaNitrosi@users.noreply.github.com>
2020-12-02 22:24:48 +0100

    Delete EsGiovedì3.py

--- EsGiovedì3.py
+++
@@ -1,8 +0,0 @@
-x = int(input("numero distanza lancio di marco"))
-y = int(input("numero distanza lancio di giacomo"))
-if x > y:
-    print("marco ha vinto con ", x)
-elif x == y:
-    print("pari")
-else:
-    print("giacomo ha vinto con ", y)
